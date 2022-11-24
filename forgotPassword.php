3ddf1d8737db6925cf39684c68029174d3c54629
tegar2804 <muhammadtegar2804@gmail.com>
2022-11-24 18:17:56 +0700

    lupa pw done

--- forgotPassword.php
+++ forgotPassword.php
@@ -1,3 +1,28 @@
+<?php
+include("config.php");
+if(isset($_POST['cont'])&&!empty($_POST['cont'])){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $passwordjuga = $_POST['passwordjuga'];
+    $data = pg_query("select * from customer where email = '".pg_escape_string($_POST['email'])."'");
+    $login_check = pg_num_rows($data);
+    if($login_check < 1){
+        header('Location: forgotPassword.php?status=terdaftar');
+    }else if($password != $passwordjuga){
+        header('Location: forgotPassword.php?status=pwsalah');
+    }else{
+        $hashpassword = md5($password);
+        $query = pg_query("UPDATE customer SET password='$hashpassword' WHERE email='$email'");
+
+	    if($query == TRUE) {
+            header('Location: login.php');
+	    } else {
+	    	header('Location: forgotPassword.php?status=gagal');
+	    }
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,15 +36,28 @@
 <body>
     <div class="container">
         <div class="login">
-            <form action="">
+            <form action="" method="POST">
                 <h1>Forgot Password</h1>
                 <hr>
-                <p>Masukkan alamat email mu</p>
-
-                <input type="text" placeholder="Masukkan email">
+                <p>Masukkan alamat email dan ganti password mu</p>
                 
+                <input type="text" name="email" placeholder="Masukkan email">
+                <?php if(isset($_GET['status']) && $_GET['status'] == 'terdaftar'): ?>
+                    <a href="register.php"> Email belum terdaftar! </a>
+                <?php endif; ?>
+
+                <input type="password" name="password" placeholder="Masukan password baru">
+                <input type="password" name="passwordjuga" placeholder="Konfirmasi password baru">
+                <?php if(isset($_GET['status']) && $_GET['status'] == 'pwsalah'): ?>
+                    <p> Password Salah! </p>
+                <?php endif; ?>
+
+                <?php if(isset($_GET['status']) && $_GET['status'] == 'gagal'): ?>
+                    <p> Gagal Mengganti Password! </p>
+                <?php endif; ?>
+                    
                 <button>
-                    <a href="newPassword.php">Continue</a>
+                    <input type="submit" value="Continue" name="cont"/>
                 </button>
             </form>
         </div>
